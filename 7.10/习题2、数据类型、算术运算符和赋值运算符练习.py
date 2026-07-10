b5eb99da094f6c4ac1582792f9d297d8a89f64c7
Dgy0527 <dgy0527@outlook.com>
2026-07-10 14:44:36 +0800

    对'7.10'补充习题2

---
+++ 7.10/习题2、数据类型、算术运算符和赋值运算符练习.py
@@ -0,0 +1,23 @@
+name='hello world' #自定义变量名name,并赋值，数据类型是字符串型
+print(name)
+print('hello world')
+
+a=6
+b=7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+
+a+=2
+print(a)
+b-=1
+print(b)
+
+x='深圳'
+y='深圳技术大学'
+z='2026'
+print(x+y)
+print(x+z)
