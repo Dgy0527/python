0279a9c714cda718f64ec9e4fdca2d5df0e20f61
Dgy0527 <dgy0527@outlook.com>
2026-07-14 18:49:35 +0800

    第一次php

---
+++ 7.14/firsttry.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+echo "你好";
+?>
+
+<?php
+$x=520;
+$y=1314;
+$z=$x+$y;
+echo $z;
+?>
+    
+</body>
+</html>
